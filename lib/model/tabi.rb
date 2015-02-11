81fcf649246e6dbc1074e99ca9a37fc19fee89cc
hitode909 <hitode909@gmail.com>
2015-02-11 17:41:53 +0900

    アクティビティ追加

--- lib/model/tabi.rb
+++ lib/model/tabi.rb
@@ -3,6 +3,7 @@ class Tabi
   include Mongoid::Timestamps
 
   field :title, type: String
+  field :activity_ids, type: Array, default: []
 
   validates :title, length: {maximum: 256, allow_blank: false}
 
@@ -13,4 +14,30 @@ class Tabi
   def edit_path
     "/tabi/#{ id }/edit"
   end
+
+  def append_activity_path
+    "/tabi/#{ id }/activities"
+  end
+
+  def activities
+    ids = { }
+    Activity.in(_id: activity_ids).each{|a|
+      ids[a.id] = a
+    }
+    activity_ids.map{|id|
+      ids[id]
+    }.compact
+  end
+
+  def append_activity(title)
+    a = Activity.new({
+        tabi_id: id,
+        title: title,
+      })
+    a.save
+    activity_ids << a.id
+    save
+    a
+  end
+
 end
